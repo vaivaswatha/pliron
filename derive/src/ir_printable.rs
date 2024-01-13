aa9fb4b0b1eea02c55df2a39c65b6d8299cf13bb
urso <steffen.siering at gmail.com>
2024-01-14 00:42:41 +0100

    common printer builder

--- derive/src/ir_printable.rs
+++ derive/src/ir_printable.rs
@@ -20,11 +20,10 @@ fn impl_struct(input: Struct) -> Result<TokenStream> {
     let name = &input.ident;
     let body = match input.kind {
         IRKind::Type | IRKind::Attribute => {
-            AttribTypePrinterBuilder2::new(input.ident.span(), &input.fields)
-                .build(&input.format)?
+            AttribTypePrinterBuilder::new(input.ident.span(), &input.fields).build(&input.format)
         }
-        IRKind::Op => OpPrinterBuilder::new(&input.fields).build(&input.format),
-    };
+        IRKind::Op => OpPrinterBuilder::new(input.ident.span(), &input.fields).build(&input.format),
+    }?;
     Ok(quote! {
         impl ::pliron::printable::Printable for #name {
             fn fmt(
@@ -40,27 +39,45 @@ fn impl_struct(input: Struct) -> Result<TokenStream> {
     })
 }
 
-struct OpPrinterBuilder {
-    fields: BTreeSet<FieldIdent>,
-}
-
-impl OpPrinterBuilder {
-    fn new(fields: &[FieldIdent]) -> Self {
-        Self {
-            fields: BTreeSet::from_iter(fields.iter().cloned()),
-        }
-    }
-
-    fn build(&self, attr: &AsmFormat) -> TokenStream {
+trait PrinterBuilder {
+    fn build(&self, attr: &AsmFormat) -> Result<TokenStream> {
         self.build_format(attr.format(), true)
     }
 
-    fn build_format(&self, format: &Format, toplevel: bool) -> TokenStream {
-        let elems = format.elems.iter();
-        elems.map(|e| self.build_elem(e, toplevel)).collect()
+    fn span(&self) -> Span;
+
+    fn finalize_toplevel(&self, value: PValue) -> Result<TokenStream>;
+
+    fn build_lit(&self, lit: &str, toplevel: bool) -> syn::Result<PValue>;
+
+    fn build_var(&self, name: &str, toplevel: bool) -> syn::Result<PValue>;
+
+    fn build_unnamed_var(&self, index: usize, toplevel: bool) -> syn::Result<PValue>;
+
+    fn build_directive(&self, d: &Directive, toplevel: bool) -> syn::Result<PValue>;
+
+    fn build_check(&self, check: &Elem) -> Result<TokenStream> {
+        let check = Directive::new_with_args("check", vec![check.clone()]);
+        let val = self.build_directive(&check, false)?;
+        match val {
+            PValue::Stream(stream) => Ok(stream),
+            PValue::Fields(fields) => Err(syn::Error::new(
+                self.span(),
+                format!("expected a literal, found `{:?}`", fields),
+            )),
+            PValue::Lit(lit) => Ok(make_print(quote! { #lit })),
+        }
     }
 
-    fn build_elem(&self, elem: &Elem, toplevel: bool) -> TokenStream {
+    fn build_format(&self, format: &Format, toplevel: bool) -> Result<TokenStream> {
+        format
+            .elems
+            .iter()
+            .map(|e| self.finalize_toplevel(self.build_elem(e, toplevel)?))
+            .collect()
+    }
+
+    fn build_elem(&self, elem: &Elem, toplevel: bool) -> Result<PValue> {
         match elem {
             Elem::Lit(Lit { lit, .. }) => self.build_lit(lit, toplevel),
             Elem::Var(Var { name, .. }) => self.build_var(name, toplevel),
@@ -70,14 +87,147 @@ impl OpPrinterBuilder {
         }
     }
 
+    fn build_optional(&self, d: &Optional, toplevel: bool) -> Result<PValue> {
+        require_toplevel(self.span(), "optional", toplevel).unwrap();
+
+        let check = self.build_check(&d.check)?;
+        let then_block = self.build_format(&d.then_format, toplevel)?;
+        if let Some(else_format) = &d.else_format {
+            let else_block = self.build_format(else_format, toplevel)?;
+            Ok(quote! {
+                if #check {
+                    #then_block
+                } else {
+                    #else_block
+                }
+            }
+            .into())
+        } else {
+            Ok(quote! {
+                if #check {
+                    #then_block
+                }
+            }
+            .into())
+        }
+    }
+}
+
+#[derive(Debug)]
+enum PValue {
+    Stream(TokenStream),
+    Fields(Vec<FieldIdent>),
+    Lit(String),
+}
+
+impl From<TokenStream> for PValue {
+    fn from(stream: TokenStream) -> Self {
+        Self::Stream(stream)
+    }
+}
+
+impl From<Vec<FieldIdent>> for PValue {
+    fn from(fields: Vec<FieldIdent>) -> Self {
+        Self::Fields(fields)
+    }
+}
+
+impl From<String> for PValue {
+    fn from(lit: String) -> Self {
+        Self::Lit(lit)
+    }
+}
+
+struct OpPrinterBuilder {
+    span: Span,
+    fields: BTreeSet<FieldIdent>,
+}
+
+impl OpPrinterBuilder {
+    fn new(span: Span, fields: &[FieldIdent]) -> Self {
+        Self {
+            span,
+            fields: BTreeSet::from_iter(fields.iter().cloned()),
+        }
+    }
+}
+
+impl PrinterBuilder for OpPrinterBuilder {
+    fn span(&self) -> Span {
+        self.span
+    }
+
+    fn finalize_toplevel(&self, value: PValue) -> Result<TokenStream> {
+        match value {
+            PValue::Stream(stream) => Ok(stream),
+            PValue::Fields(fields) => Err(syn::Error::new(
+                self.span,
+                format!("expected a literal, found `{:?}`", fields),
+            )),
+            PValue::Lit(lit) => Ok(make_print(quote! {
+                ::pliron::asmfmt::printers::literal(#lit)
+            })),
+        }
+    }
+
+    fn build_lit(&self, lit: &str, toplevel: bool) -> Result<PValue> {
+        let toks = if toplevel {
+            make_print(quote! {
+                ::pliron::asmfmt::printers::literal(#lit)
+            })
+        } else {
+            quote! { #lit }
+        };
+        Ok(toks.into())
+    }
+
+    fn build_var(&self, name: &str, toplevel: bool) -> Result<PValue> {
+        let ident = FieldIdent::Named(name.into());
+        if self.fields.contains(&ident) {
+            return Ok(self.build_field_use(ident, toplevel).into());
+        }
+        Ok(self.build_attribute_use(name, toplevel).into())
+    }
+
+    fn build_unnamed_var(&self, index: usize, toplevel: bool) -> Result<PValue> {
+        Ok(self
+            .build_field_use(syn::Index::from(index), toplevel)
+            .into())
+    }
+
+    fn build_directive(&self, d: &Directive, toplevel: bool) -> Result<PValue> {
+        let directive = format_ident!("op_{}_directive", d.name);
+        let args = d
+            .args
+            .iter()
+            .map(|a| match self.build_elem(a, false)? {
+                PValue::Stream(stream) => Ok(stream),
+                PValue::Fields(fields) => Err(syn::Error::new(
+                    self.span(),
+                    format!("expected tokens, but found struct fields `{:?}`", fields),
+                )),
+                PValue::Lit(lit) => Err(syn::Error::new(
+                    self.span(),
+                    format!("expected a tokens, but found a string value `{}`", lit),
+                )),
+            })
+            .collect::<Result<Vec<_>>>()?;
+        let printer = quote! {
+            ::pliron::asmfmt::printers::#directive!(ctx, self #(, #args)*)
+        };
+        Ok(make_print_if(toplevel, printer).into())
+    }
+}
+
+impl OpPrinterBuilder {
     fn build_field_use<T>(&self, ident: T, toplevel: bool) -> TokenStream
     where
         T: quote::ToTokens,
     {
         if toplevel {
-            quote! {
-                ::pliron::asmfmt::printers::print_var!(&self.#ident).fmt(ctx, state, fmt)?;
-            }
+            make_print(quote! {
+                ::pliron::asmfmt::printers::print_var!(&self.#ident)
+            })
         } else {
             quote! {
                 #ident
@@ -86,176 +236,73 @@ impl OpPrinterBuilder {
     }
 
     fn build_attribute_use(&self, name: &str, toplevel: bool) -> TokenStream {
-        let mut printer = quote! {
+        let printer = quote! {
             ::pliron::asmfmt::printers::get_attr!(self, #name)
         };
-        if toplevel {
-            printer = quote! { #printer.fmt(ctx, state, fmt)?; };
-        }
-        printer
-    }
-
-    fn build_lit(&self, lit: &str, toplevel: bool) -> TokenStream {
-        if toplevel {
-            quote! {
-                ::pliron::asmfmt::printers::literal(#lit).fmt(ctx, state, fmt)?;
-            }
-        } else {
-            quote! {
-                #lit
-            }
-        }
-    }
-
-    fn build_var(&self, name: &str, toplevel: bool) -> TokenStream {
-        let ident = FieldIdent::Named(name.into());
-        if self.fields.contains(&ident) {
-            return self.build_field_use(ident, toplevel);
-        }
-        self.build_attribute_use(name, toplevel)
-    }
-
-    fn build_unnamed_var(&self, index: usize, toplevel: bool) -> TokenStream {
-        self.build_field_use(syn::Index::from(index), toplevel)
-    }
-
-    fn build_directive(&self, d: &Directive, toplevel: bool) -> TokenStream {
-        let directive = format_ident!("op_{}_directive", d.name);
-        let args = d.args.iter().map(|a| self.build_elem(a, false));
-        let mut printer = quote! {
-            ::pliron::asmfmt::printers::#directive!(ctx, self #(, #args)*)
-        };
-        if toplevel {
-            printer = quote! { #printer.fmt(ctx, state, fmt)?; };
-        }
-        printer
-    }
-
-    fn build_optional(&self, d: &Optional, toplevel: bool) -> TokenStream {
-        let check = Directive::new_with_args("check", vec![d.check.as_ref().clone()]);
-
-        let check = self.build_directive(&check, false);
-        let then_block = self.build_format(&d.then_format, toplevel);
-        if let Some(else_format) = &d.else_format {
-            let else_block = self.build_format(else_format, toplevel);
-            quote! {
-                if #check {
-                    #then_block
-                } else {
-                    #else_block
-                }
-            }
-        } else {
-            quote! {
-                if #check {
-                    #then_block
-                }
-            }
-        }
+        make_print_if(toplevel, printer)
     }
 }
 
-struct AttribTypePrinterBuilder2<'a> {
+struct AttribTypePrinterBuilder<'a> {
     span: Span,
     fields: &'a [FieldIdent],
 }
 
-#[derive(Debug)]
-enum ATValue {
-    Stream(TokenStream),
-    Fields(Vec<FieldIdent>),
-    Lit(String),
-}
-
-impl From<TokenStream> for ATValue {
-    fn from(stream: TokenStream) -> Self {
-        Self::Stream(stream)
-    }
-}
-
-impl From<Vec<FieldIdent>> for ATValue {
-    fn from(fields: Vec<FieldIdent>) -> Self {
-        Self::Fields(fields)
-    }
-}
-
-impl From<String> for ATValue {
-    fn from(lit: String) -> Self {
-        Self::Lit(lit)
-    }
-}
-
-impl<'a> AttribTypePrinterBuilder2<'a> {
+impl<'a> AttribTypePrinterBuilder<'a> {
     fn new(span: Span, fields: &'a [FieldIdent]) -> Self {
         Self { span, fields }
     }
+}
 
-    fn build(&self, attr: &AsmFormat) -> syn::Result<TokenStream> {
-        self.build_format(attr.format(), true)
+impl<'a> PrinterBuilder for AttribTypePrinterBuilder<'a> {
+    fn span(&self) -> Span {
+        self.span
     }
 
-    fn build_format(&self, format: &Format, toplevel: bool) -> syn::Result<TokenStream> {
-        let elems = format.elems.iter();
-        elems
-            .map(|e| match self.build_elem(e, toplevel)? {
-                ATValue::Stream(stream) => Ok(stream),
-                ATValue::Fields(fields) => Err(syn::Error::new(
-                    self.span,
-                    format!("expected a literal, found `{:?}`", fields),
-                )),
-                ATValue::Lit(lit) => Ok(quote! {
-                    #lit.fmt(ctx, state, fmt)?;
-                }),
-            })
-            .collect()
-    }
-
-    fn build_elem(&self, elem: &Elem, toplevel: bool) -> syn::Result<ATValue> {
-        match elem {
-            Elem::Lit(Lit { lit, .. }) => self.build_lit(lit, toplevel),
-            Elem::Var(Var { name, .. }) => self.build_var(name, toplevel),
-            Elem::UnnamedVar(UnnamedVar { index, .. }) => self.build_unnamed_var(*index, toplevel),
-            Elem::Directive(ref d) => self.build_directive(d, toplevel),
-            Elem::Optional(ref opt) => self.build_optional(opt, toplevel),
+    fn finalize_toplevel(&self, value: PValue) -> Result<TokenStream> {
+        match value {
+            PValue::Stream(stream) => Ok(stream),
+            PValue::Fields(fields) => Err(syn::Error::new(
+                self.span,
+                format!("expected a literal, found `{:?}`", fields),
+            )),
+            PValue::Lit(lit) => Ok(make_print(quote! { #lit })),
         }
     }
 
-    fn build_lit(&self, lit: &str, toplevel: bool) -> syn::Result<ATValue> {
+    fn build_lit(&self, lit: &str, toplevel: bool) -> syn::Result<PValue> {
         if toplevel {
-            Ok(quote! {
-                #lit.fmt(ctx, state, fmt)?;
-            }
-            .into())
+            Ok(make_print(quote! { #lit }).into())
         } else {
             Ok(lit.to_string().into())
         }
     }
 
-    fn build_var(&self, name: &str, toplevel: bool) -> syn::Result<ATValue> {
+    fn build_var(&self, name: &str, toplevel: bool) -> syn::Result<PValue> {
         if toplevel {
             let ident = format_ident!("{}", name);
-            Ok(quote! {
-                ::pliron::asmfmt::printers::print_var!(&self.#ident).fmt(ctx, state, fmt)?;
-            }
+            Ok(make_print(quote! {
+                ::pliron::asmfmt::printers::print_var!(&self.#ident)
+            })
             .into())
         } else {
-            Ok(ATValue::Fields(vec![name.into()]))
+            Ok(PValue::Fields(vec![name.into()]))
         }
     }
 
-    fn build_unnamed_var(&self, index: usize, toplevel: bool) -> syn::Result<ATValue> {
+    fn build_unnamed_var(&self, index: usize, toplevel: bool) -> syn::Result<PValue> {
         if toplevel {
             let ident = syn::Index::from(index);
-            Ok(quote! {
-                ::pliron::asmfmt::printers::print_var!(&self.#ident).fmt(ctx, state, fmt)?;
-            }
+            Ok(make_print(quote! {
+                ::pliron::asmfmt::printers::print_var!(&self.#ident)
+            })
             .into())
         } else {
-            Ok(ATValue::Fields(vec![index.into()]))
+            Ok(PValue::Fields(vec![index.into()]))
         }
     }
 
-    fn build_directive(&self, d: &Directive, toplevel: bool) -> syn::Result<ATValue> {
+    fn build_directive(&self, d: &Directive, toplevel: bool) -> syn::Result<PValue> {
         match d.name.as_str() {
             "params" => {
                 require_no_args(self.span, "params", &d.args)?;
@@ -263,7 +310,7 @@ impl<'a> AttribTypePrinterBuilder2<'a> {
                     let args = quote! { () };
                     Ok(self.build_call_toplevel_directive(d, args).into())
                 } else {
-                    Ok(ATValue::Fields(self.fields.iter().cloned().collect()))
+                    Ok(PValue::Fields(self.fields.iter().cloned().collect()))
                 }
             }
             "struct" => {
@@ -287,7 +334,9 @@ impl<'a> AttribTypePrinterBuilder2<'a> {
             }
         }
     }
+}
 
+impl<'a> AttribTypePrinterBuilder<'a> {
     fn build_call_toplevel_directive(&self, d: &Directive, args: TokenStream) -> TokenStream {
         let printer_args = quote! { (ctx, state, fmt) };
         let field_names = self.fields;
@@ -297,12 +346,12 @@ impl<'a> AttribTypePrinterBuilder2<'a> {
         }
     }
 
-    fn build_arg_fields(&self, args: &[Elem]) -> syn::Result<Vec<ATValue>> {
+    fn build_arg_fields(&self, args: &[Elem]) -> syn::Result<Vec<PValue>> {
         let mut fields = vec![];
         for (i, arg) in args.iter().enumerate() {
             let v = self.build_elem(arg, false)?;
             match v {
-                ATValue::Stream(_) => {
+                PValue::Stream(_) => {
                     return Err(syn::Error::new(
                         self.span,
                         format!("argument {} is not a valid argument", i),
@@ -314,50 +363,23 @@ impl<'a> AttribTypePrinterBuilder2<'a> {
         Ok(fields)
     }
 
-    fn args_to_token_streams(&self, args: &[ATValue]) -> TokenStream {
+    fn args_to_token_streams(&self, args: &[PValue]) -> TokenStream {
         let mut streams = vec![];
         for arg in args {
             match arg {
-                ATValue::Stream(_) => unreachable!(),
-                ATValue::Fields(fields) => {
+                PValue::Stream(_) => unreachable!(),
+                PValue::Fields(fields) => {
                     streams.extend(fields.iter().map(|f| {
                         quote! { #f }
                     }));
                 }
-                ATValue::Lit(lit) => {
+                PValue::Lit(lit) => {
                     streams.push(quote! { #lit });
                 }
             }
         }
         quote! {
             (#(#streams),*)
-        }
-    }
-
-    fn build_optional(&self, d: &Optional, toplevel: bool) -> syn::Result<ATValue> {
-        require_toplevel(self.span, "optional", toplevel).unwrap();
-
-        let check = Directive::new_with_args("check", vec![d.check.as_ref().clone()]);
-
-        let check = require_tokenstream(self.span, self.build_directive(&check, false)?)?;
-        let then_block = self.build_format(&d.then_format, toplevel)?;
-        if let Some(else_format) = &d.else_format {
-            let else_block = self.build_format(else_format, toplevel)?;
-            Ok(quote! {
-                if #check {
-                    #then_block
-                } else {
-                    #else_block
-                }
-            }
-            .into())
-        } else {
-            Ok(quote! {
-                if #check {
-                    #then_block
-                }
-            }
-            .into())
         }
     }
 }
@@ -392,15 +414,16 @@ fn require_args(span: Span, directive: &str, args: &[Elem]) -> syn::Result<()> {
     Ok(())
 }
 
-fn require_tokenstream(span: Span, val: ATValue) -> syn::Result<TokenStream> {
-    match val {
-        ATValue::Stream(stream) => Ok(stream),
-        ATValue::Fields(fields) => Err(syn::Error::new(
-            span,
-            format!("expected a literal, found `{:?}`", fields),
-        )),
-        ATValue::Lit(lit) => Ok(quote! {
-            #lit.fmt(ctx, state, fmt)?
-        }),
+fn make_print(stmt: TokenStream) -> TokenStream {
+    quote! {
+        #stmt.fmt(ctx, state, fmt)?;
+    }
+}
+
+fn make_print_if(cond: bool, stmt: TokenStream) -> TokenStream {
+    if cond {
+        make_print(stmt)
+    } else {
+        stmt
     }
 }
