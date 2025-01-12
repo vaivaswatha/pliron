8012a99441c5f8c8f66fb941dcc418a218cd7079
Vaivaswatha N <vaivaswatha@gmail.com>
2025-01-12 22:07:01 +0530

    [pliron-derive] minor error handling for enum variants format directive

--- pliron-derive/src/irfmt/mod.rs
+++ pliron-derive/src/irfmt/mod.rs
@@ -83,15 +83,25 @@ impl Enum {
             data.variants
                 .iter()
                 .map(|v| {
+                    if v.attrs.len() > 1 {
+                        return Err(syn::Error::new_spanned(
+                            &name,
+                            "Enum variants are allowed one #[format = \"...\"] attribute, and no others",
+                        ));
+                    }
                     // parse the variant's format string given as an attribute
                     let fmt_str = v
                         .attrs
                         .iter()
-                        .find_map(|attr| {
+                        .next()
+                        .map(|attr| {
                             if attr.path().is_ident("format") {
-                                Some(attr.parse_args::<LitStr>())
+                                attr.parse_args::<LitStr>()
                             } else {
-                                None
+                                Err(syn::Error::new_spanned(
+                                    &name,
+                                    "Enum variants can only have a #[format = \"...\"] attribute",
+                                ))
                             }
                         })
                         .transpose()?;
